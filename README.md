ec82dd29466cc3b9dc9522ee266ec918f621fa18
Soichi Hayashi <hayashis@iu.edu>
2016-04-15 17:29:03 +0000

    added a bit more meat

--- README.md
+++ README.md
@@ -3,6 +3,22 @@ Curl downloader service for SCA.
 
 It simply downloads files listed in the config.json via curl and create products.json for files successfully downloaded
 
-TODO..
+* Installation
 
-Report to progress service for each file downloaded (use filesize?)
+You probably need to install "pip install request"
+
+* Input 
+
+config.json
+```
+{
+    "download: [
+        {"url": "http://example.com/some/file.txt"},
+        {"url": "http://example.com/another/file.txt"},
+    ]
+}
+```
+
+Upon execution, files will be downloaded under ./download
+
+TODO..  Report to progress service for each file downloaded (use filesize?)
---
+++ run.py
@@ -0,0 +1,65 @@
+#!/bin/env python
+
+import os
+import json
+import urllib2
+import requests
+import time
+
+block_sz = 8192*10
+
+with open('config.json') as config_json:
+    config = json.load(config_json)
+
+
+idx=0
+for file in config["download"]:
+
+    dir=file["dir"]
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    
+    url = file["url"]
+    u = urllib2.urlopen(url)
+    file_name = url.split('/')[-1]
+    f = open(dir+"/"+file_name, 'w')
+    meta = u.info()
+    file_size = int(meta.getheaders("Content-Length")[0])
+
+    progress_url = os.environ["SCA_PROGRESS_URL"]+".file"+str(idx);
+    requests.post(progress_url, json={"status": "running", "progress": 0, "msg": "downloading "+url+" ("+str(file_size)+" bytes)"});
+
+    print "Downloading: %s Bytes: %s" % (file_name, file_size)
+
+    file_size_dl = 0
+    progress_time = time.time()
+    while True:
+        buffer = u.read(block_sz)
+        if not buffer:
+            break
+
+        file_size_dl += len(buffer)
+        f.write(buffer)
+
+        if time.time() - progress_time > 0.5:
+            #print time.time()
+            requests.post(progress_url, json={"progress": float(file_size_dl)/file_size})
+            progress_time = time.time()
+            status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+            print status
+        #status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+        #status = status + chr(8)*(len(status)+1) #what is this?
+        #print status
+
+    requests.post(progress_url, json={"progress": 1, "status": "finished"});
+
+    f.close()
+    idx+=1
+
+    #filesize=`curl -sI $url | grep Content-Length | cut -f 2 -d " "`
+#
+#echo "filesize: $filesize"
+#echo "downloding $url"
+#(cd download && curl -O $url)
+#
+##echo '[{"filename":"download/10142_3_MPRAGE_online.nii", "size":' $filesize '}]' > products.json
