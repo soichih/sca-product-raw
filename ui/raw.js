58641ebf073525f20ac4610f4d407a5e30979dd6
Soichi Hayashi <soichih@gmail.com>
2016-06-22 21:32:17 +0000

    updated the way file/dirs are displayed

--- ui/raw.js
+++ ui/raw.js
@@ -13,48 +13,64 @@
             }, 
             templateUrl: 'bower_components/sca-product-raw/ui/raw.html',
             link: function($scope, element) {
-                $scope.jwt = localStorage.getItem(appconf.jwt_id);
-                $scope.appconf = appconf; //used by template
-                if(!$scope.path) $scope.path = "";
-
-                var file_timeout;
-                $scope.t = scaTask.get($scope.taskid);
-                $scope.t._promise.then(function() {
-                    $scope.taskdir = $scope.t.instance_id+"/"+$scope.t._id;
-
-                    //reload files if resource_id is set
-                    if($scope.t.resource_id) {
-                        $scope.loading = true;
-                        //$scope.path = $scope.path || $scope.t.instance_id+"/"+$scope.t._id;
-                        //$scope.path = $scope.t.instance_id+"/"+$scope.t._id;
-                        $http.get(appconf.sca_api+"/resource/ls/"+$scope.t.resource_id, {
-                            //timeout: 3000, //server side should handle this (with good explanation)
-                            params: {
-                                path: $scope.taskdir+"/"+$scope.path,
-                            }
-                        })
-                        .then(function(res) {
-                            $scope.loading = false;
-                            $scope.files = res.data.files;
-                            $scope.files.forEach(function(file) {
-                                file.path = $scope.path+"/"+file.filename;
-                            });
-                        }, function(res) {
-                            $scope.loading = false;
-                            if(res.data && res.data.message) toaster.error(res.data.message);
-                            else toaster.error(res.statusText);
-                        });
-                    } else {
-                        console.log("resource_id not loaded yet..");
-                    }
-                });
-
-                $scope.$on("$destroy", function(event) {
-                    if(file_timeout) $timeout.cancel(file_timeout);
-                });
             }
         };
     }]);
+
+    service.component('scaProductRawDir', {
+        //transclude: true,
+        bindings: {
+            taskid: '=',
+            indent: '=',
+            path: '=?', //if empty, it will be set to instantce_id / task_id (relative to task dir)
+        },
+        templateUrl: 'bower_components/sca-product-raw/ui/dir.html',
+        controller: function($scope, appconf, $http, toaster, scaTask) {
+            var $ctrl = this;
+            var jwt = localStorage.getItem(appconf.jwt_id);
+            this.indent_child = this.indent+1;
+            if(!this.path) this.path = "";
+
+            this.getNumber = function(num) { return new Array(num); };
+
+            //start by loading task
+            var t = scaTask.get(this.taskid);
+            t._promise.then(function() {
+                $ctrl.taskdir = t.instance_id+"/"+t._id;
+                if(!t.resource_id) {
+                    console.error("sca-product-raw initialized on task with no resource_id");
+                    return;
+                }
+
+                $ctrl.loading = true;
+                console.log("loading from resource:"+t.resource_id+" path:"+$ctrl.taskdir+"/"+$ctrl.path);
+                $http.get(appconf.sca_api+"/resource/ls/"+t.resource_id, {
+                    //timeout: 3000, //server side should handle this (with good explanation)
+                    params: { path: $ctrl.taskdir+"/"+$ctrl.path }
+                })
+                .then(function(res) {
+                    $ctrl.loading = false;
+                    $ctrl.files = res.data.files;
+                    $ctrl.files.forEach(function(file) {
+                        file.path = $ctrl.path+"/"+file.filename;
+                        file.url = appconf.sca_api+"/resource/download?r="+t.resource_id+"&p="+encodeURIComponent($ctrl.taskdir+"/"+file.path)+"&at="+jwt;
+                    });
+                    console.dir($ctrl.files);
+                }, function(res) {
+                    $ctrl.loading = false;
+                    if(res.data && res.data.message) toaster.error(res.data.message);
+                    else toaster.error(res.statusText);
+                });
+            });
+
+            /*
+            //TODO - it this called for component?
+            $scope.$on("$destroy", function(event) {
+                if(file_timeout) $timeout.cancel(file_timeout);
+            });
+            */
+        }
+    });
 
     //can't quite do the slidedown animation through pure angular/css.. borrowing slideDown from jQuery..
     service.animation('.slide-down', ['$animateCss', function($animateCss) {
