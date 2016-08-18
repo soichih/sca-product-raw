47d438bf9f83ca58458aab6a631b5b353d18fafd
Soichi Hayashi <soichih@gmail.com>
2016-08-18 21:42:24 +0000

    added comment

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#need to reset the LANG to avoid following error from tarfile
+#'ascii' codec can't decode byte 0xc3 in position 1: ordinal not in range(128)
+#I tried setting environ["LANG"] and tarfile.ENCODING inside import.py, but that didn't fix it
 export LANG="en_US.UTF-8"
+
 $SCA_SERVICE_DIR/import.py
 
