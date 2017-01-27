dfac4d0edd440f3786a88230f1f7ab309795567d
Soichi Hayashi <hayashis@login1.bigred2.uits.iu.edu>
2017-01-26 19:33:23 -0500

    added debugging stub

--- start.sh
+++ start.sh
@@ -5,6 +5,12 @@
 #I tried setting environ["LANG"] and tarfile.ENCODING inside main.py, but that didn't fix it
 export LANG="en_US.UTF-8"
 
+#allows test execution
+if [ -z $SCA_WORKFLOW_DIR ]; then export SCA_WORKFLOW_DIR=`pwd`; fi
+if [ -z $SCA_TASK_DIR ]; then export SCA_TASK_DIR=`pwd`; fi
+if [ -z $SCA_SERVICE_DIR ]; then export SCA_SERVICE_DIR=`pwd`; fi
+if [ -z $SCA_PROGRESS_URL ]; then export SCA_PROGRESS_URL="https://soichi7.ppa.iu.edu/api/progress/status/_sca.test"; fi
+
 #do bigred2 specific things
 echo $HOME | grep -i bigred > /dev/null
 if [ $? -eq 0 ]; then
